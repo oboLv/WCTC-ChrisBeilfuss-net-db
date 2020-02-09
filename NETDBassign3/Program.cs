345a019f56149940ee2089ea50cc1a7e506ea0b8
oboLv <60120705+oboLv@users.noreply.github.com>
2020-02-08 19:13:09 -0600

    Finished population code

--- NETDBassign3/Program.cs
+++ NETDBassign3/Program.cs
@@ -10,10 +10,11 @@ namespace NETDBassign3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. View All Movies");
+            Console.WriteLine("1. View Movies");
             Console.WriteLine("2. Add Movie");
+            Console.WriteLine("3. Exit App");
             string userInput = Console.ReadLine();
-            //populate dictionaries
+            //dictionaries/lists
             Dictionary<int, int> movieIDs = new Dictionary<int, int>();
             Dictionary<int, string> movieTitles = new Dictionary<int, string>();
             List<int> action = new List<int>();
@@ -36,9 +37,110 @@ namespace NETDBassign3
             List<int> mystery = new List<int>();
 
             StreamReader sR = new StreamReader("movies.csv");
-            while(!sR.EndOfStream)
+            while(!sR.EndOfStream)//populate dictionaries
             {
+                string rough = sR.ReadLine();
+                string[] split = rough.Split(',');
 
+                int splitLength = split.Length();//fix broken titles
+                bool brokenTitle = false;
+                string fixedTitle = "";
+                if(splitLength > 3)
+                    {
+                        for(int i = 1; i <= splitLength-1; i++)
+                        {
+                            fixedTitle = $"{fixedTitle} + {split[i]}";
+                        }
+                        brokenTitle = true;
+                    }
+
+                string[] genres = split[splitLength - 1].Split('|');
+                int movieID = Convert.ToInt32(split[0]);
+                movieIDs.Add(movieID, movieID);
+                if(!brokenTitle)
+                    {
+                        movieTitles.Add(movieID, fixedTitle);
+                    }
+                else
+                    {
+                        movieTitles.Add(movieID, split[1]);
+                    }
+                foreach(string genre in genres) //sort movies into genres
+                    {
+                        if(genres.Contains<string>("Action"))
+                        {
+                            action.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Adventure"))
+                        {
+                            adventure.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Drama"))
+                        {
+                            drama.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Comedy"))
+                        {
+                            comedy.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Musical"))
+                        {
+                            musical.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Fantasy"))
+                        {
+                            fantasy.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Sci-Fi"))
+                        {
+                            scifi.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Thriller"))
+                        {
+                            thriller.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Children"))
+                        {
+                            children.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Horror"))
+                        {
+                            horror.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Animation"))
+                        {
+                            animation.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Crime"))
+                        {
+                            crime.Add(movieID);
+                        }
+                        if(genres.Contains<string>("War"))
+                        {
+                            war.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Film-Noir"))
+                        {
+                            noir.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Western"))
+                        {
+                            western.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Documentary"))
+                        {
+                            documentary.Add(movieID);
+                        }
+                        if(genres.Contains<string>("IMAX"))
+                        {
+                            imax.Add(movieID);
+                        }
+                        if(genres.Contains<string>("Mystery"))
+                        {
+                            mystery.Add(movieID);
+                        }
+                        
+                    }
             }
 
             if(userInput == "1")
